791151a0946244b342c73807b7235a56bc68604e
Neaj Mahmud <neajmahmud.dev@gmail.com>
2025-02-17 00:12:02 -0500

    adding new details

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,41 +1,62 @@
 import { useNavigate } from "react-router-dom";
-
+import { useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
+import ItemDetail from "./ItemDetail";
 import "./item.css";
-const Item = ({ guid, name, prices, imageUrls }) => {
+const Item = ({ guid, name, prices, imageUrls, description, ...props }) => {
+  console.log("props: ", props);
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   const navigate = useNavigate();
   const handleNavigation = () => navigate(`/item/${guid}`);
   const dummyImage =
     "https://cdn2.picryl.com/photo/1988/12/01/good-food-display-nci-visuals-online-bb10ce-1024.jpg";
   return (
-    <Card className="animate__animated animate__fadeIn" raised>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="260"
-          image={imageUrls?.large || dummyImage}
-          alt={guid}
-          onClick={handleNavigation}
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary" noWrap>
-            {name}
+    <>
+      <ItemDetail open={open} handleClose={handleClose} />
+      <Card
+        onClick={handleClickOpen}
+        className="animate__animated animate__fadeIn"
+        raised
+      >
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="260"
+            image={imageUrls?.large || dummyImage}
+            alt={guid}
+            onClick={handleClickOpen}
+          />
+          <CardContent sx={{ height: 200 }}>
+            <Typography variant="h5" color="text.secondary">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {description}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions sx={{ display: "flex", justifyContent: "space-around" }}>
+          {/* <Button size="small" color="error" onClick={handleNavigation}>
+            More Info
+          </Button> */}
+          <Typography variant="subtitle2" color="text.secondary" align="right">
+            {`$${prices[0]}`}
           </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions sx={{ display: "flex", justifyContent: "space-around" }}>
-        <Button size="small" color="error" onClick={handleNavigation}>
-          More Info
-        </Button>
-        <Typography variant="subtitle2" color="text.secondary" align="right">
-          {`$${prices[0]}`}
-        </Typography>
-      </CardActions>
-    </Card>
+        </CardActions>
+      </Card>
+    </>
   );
 };
 
--- src/components/Item/ItemDetail.jsx
+++ src/components/Item/ItemDetail.jsx
@@ -1,72 +1,124 @@
-import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+// import { useContext, useState } from "react";
+// import { Link } from "react-router-dom";
 
-import { CartContext } from "../../context/CartContext";
-import ItemDescription from "./ItemDescription";
-import ItemCount from "./ItemCount";
+// import { CartContext } from "../../context/CartContext";
+// import ItemDescription from "./ItemDescription";
+// import ItemCount from "./ItemCount";
 
-import SuccessSnackbar from "../ui/SuccessSnackbar";
-import GoBackBtn from "../ui/GoBackBtn";
+// import SuccessSnackbar from "../ui/SuccessSnackbar";
+// import GoBackBtn from "../ui/GoBackBtn";
 
-import Grid from "@mui/material/Grid";
+// import Grid from "@mui/material/Grid";
+// import Button from "@mui/material/Button";
+// import Typography from "@mui/material/Typography";
+// import Card from "@mui/material/Card";
+// import CardMedia from "@mui/material/CardMedia";
+// import Divider from "@mui/material/Divider";
+// import Box from "@mui/material/Box";
+// import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+// import Gallery from "./Components/Gallery";
+// import Description from "./Components/Description";
+// import MobileGallery from "./Components/MobileGallery";
+// import GalleryView from "./Gallery/GalleryView";
+// import Navbar from "../NavBar/NavBar";
+
+// const ItemDetail = ({ id, title, description, price, stock, image }) => {
+//   const { addItemToCart, isInCart } = useContext(CartContext);
+//   const [showSuccessBar, setShowSuccessBar] = useState(false);
+//   const imgPath = `/assets/img/${id}.jpg`;
+//   const [quant, setQuant] = useState(1);
+//   const [orderedQuant, setOrderedQuant] = useState(0);
+
+//   const addQuant = () => {
+//     setQuant(quant + 1);
+//   };
+
+//   const removeQuant = () => {
+//     setQuant(quant - 1);
+//   };
+
+//   const resetQuant = () => {
+//     setQuant(0);
+//     setOrderedQuant(0);
+//   };
+
+//   const handleAddItemToCart = (quantity) => {
+//     if (isInCart(id) || quantity === 0) return;
+
+//     addItemToCart({ id, title, price, quantity });
+//     setShowSuccessBar(true);
+//   };
+
+//   return (
+//     <>
+//       <Navbar />
+//       <section className="core">
+//         <Gallery />
+//         <MobileGallery />
+
+//         <Description
+//           onQuant={quant}
+//           onAdd={addQuant}
+//           onRemove={removeQuant}
+//           onSetOrderedQuant={setOrderedQuant}
+//           handleAddItemToCart={handleAddItemToCart}
+//         />
+//       </section>
+//       {showSuccessBar && <SuccessSnackbar message={"Product added to cart"} />}
+//     </>
+//   );
+// };
+
+// export default ItemDetail;
+
+import * as React from "react";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import Card from "@mui/material/Card";
-import CardMedia from "@mui/material/CardMedia";
+import Dialog from "@mui/material/Dialog";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemButton from "@mui/material/ListItemButton";
+import List from "@mui/material/List";
 import Divider from "@mui/material/Divider";
-import Box from "@mui/material/Box";
-import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
-import Gallery from "./Components/Gallery";
-import Description from "./Components/Description";
-import MobileGallery from "./Components/MobileGallery";
-import GalleryView from "./Gallery/GalleryView";
-import Navbar from "../NavBar/NavBar";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+import SelectActionCard from "./Components/MultiCardSelect";
 
-const ItemDetail = ({ id, title, description, price, stock, image }) => {
-  const { addItemToCart, isInCart } = useContext(CartContext);
-  const [showSuccessBar, setShowSuccessBar] = useState(false);
-  const imgPath = `/assets/img/${id}.jpg`;
-  const [quant, setQuant] = useState(1);
-  const [orderedQuant, setOrderedQuant] = useState(0);
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
-  const addQuant = () => {
-    setQuant(quant + 1);
-  };
-
-  const removeQuant = () => {
-    setQuant(quant - 1);
-  };
-
-  const resetQuant = () => {
-    setQuant(0);
-    setOrderedQuant(0);
-  };
-
-  const handleAddItemToCart = (quantity) => {
-    if (isInCart(id) || quantity === 0) return;
-
-    addItemToCart({ id, title, price, quantity });
-    setShowSuccessBar(true);
-  };
-
+export default function ItemDetail({ open, handleClose }) {
   return (
-    <>
-      <Navbar />
-      <section className="core">
-        <Gallery />
-        <MobileGallery />
-
-        <Description
-          onQuant={quant}
-          onAdd={addQuant}
-          onRemove={removeQuant}
-          onSetOrderedQuant={setOrderedQuant}
-          handleAddItemToCart={handleAddItemToCart}
-        />
-      </section>
-      {showSuccessBar && <SuccessSnackbar message={"Product added to cart"} />}
-    </>
+    <React.Fragment>
+      <Dialog
+        fullScreen
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: "relative" }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              Sound
+            </Typography>
+            <Button autoFocus color="inherit" onClick={handleClose}>
+              save
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <SelectActionCard />
+      </Dialog>
+    </React.Fragment>
   );
-};
-
-export default ItemDetail;
+}
